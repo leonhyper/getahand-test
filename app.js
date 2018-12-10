cb803d424ec01707f936221fdf6d359ff175b75c
zhusheng <1013702057@qq.com>
2018-12-10 23:35:41 +0800

    New Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV |= 'test') {
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Origin', 'http://outstanding-whistle.surge.sh');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials','true');
